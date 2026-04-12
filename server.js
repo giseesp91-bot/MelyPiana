6d3c8537fef0abff7d21a80707ee72349c454e81
Gise <gis.eesp91@gmail.com>
2026-04-11 22:10:06 -0300

    primera version tienda

---
+++ server.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+
+console.log("TOKEN:", process.env.MP_ACCESS_TOKEN);
+
+const express = require("express");
+const cors = require("cors");
+const mercadopago = require("mercadopago");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// 🔐 reemplazá esto después con tu access token real
+const client = new mercadopago.MercadoPagoConfig({
+  accessToken:process.env.MP_ACCESS_TOKEN
+});
+
+app.post("/crear-preferencia", async (req, res) => {
+  try {
+    const {carrito, cliente} = req.body;
+    
+    console.log("CLIENTE:", cliente);
+    console.log("CARRITO:", carrito);
+
+    const items = carrito.map(producto => ({
+      title: `${producto.nombre} (Talle: ${producto.talle})`,
+      quantity: 1,
+      unit_price: producto.precio,
+      currency_id: "ARS",
+      description: "Producto de Tienda Mely"
+    }));
+
+    const preference = {
+      items,
+    };
+
+    const preferenceClient = new mercadopago.Preference(client);
+
+const respuesta = await preferenceClient.create({
+  body: preference
+});
+
+    res.json({ init_point: respuesta.init_point });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error al crear pago");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000");
+});
